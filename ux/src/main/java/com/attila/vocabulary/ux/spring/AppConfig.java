a78841c4961358194d8c1c68d75ba64b598ed993
attila.deak <deakattila87@gmail.com>
2016-06-11 23:58:44 +0300

    original structure

---
+++ ux/src/main/java/com/attila/vocabulary/ux/spring/AppConfig.java
@@ -0,0 +1,18 @@
+package com.attila.vocabulary.ux.spring;
+
+import com.attila.vocabulary.ux.spring.PersistenceConfig;
+import org.springframework.context.annotation.*;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+@ComponentScan
+@Import(PersistenceConfig.class)
+@PropertySource("classpath:app.properties")
+public class AppConfig {
+
+    @Bean
+    public PropertySourcesPlaceholderConfigurer propertyConfigIn() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
+}
